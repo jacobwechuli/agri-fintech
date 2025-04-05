72c3d9892b14b158d5af1f57390e3bb7f4339187
Jacob Wechuli <wechulijac@gmail.com>
2025-04-05 22:55:43 +0300

    Initial project setup

---
+++ src/main/java/com/sgrifintech/loan_management_service/LoanManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sgrifintech.loan_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgrifintech/loan_management_service/LoanManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgrifintech.loan_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
